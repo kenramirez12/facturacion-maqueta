e83493288147019b97677ad3abd6e1e28f57f65f
Ken Ramirez <kenramirezp@gmail.com>
2018-04-17 09:09:38 -0500

    funciones agregar y eliminar producto desarrollada

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+/** Menu Toggle */
 toggleMenuBtn = $('.main-header__menu-btn');
 header = $('.main-header');
 headerLogo = $('.main-header__logo');
@@ -39,7 +40,7 @@ const toggleClientMenu = function() {
     }
 }
 
-// Switch checkbox
+/** Switch buttons */
 $('.switch').click(function(e) {
     e.preventDefault();
 
@@ -73,46 +74,58 @@ if($('.select-field')) {
 }
 
 
-// Select default modified
-// $(document).ready(function() {
-    if($('.select-default').length > 0) {
-      $('.select-default').each(function() {
-        var defaultVal = $(this).data('default'),
+/** Select Definitivo */
+const initSelect = function() {
+    $('.select-default').each(function() {
+        var defaultValue = $(this).data('default'),
+            realValue = $(this).find($('.select__hidden')).val();
             selected = $(this).children('.select-default__selected');
         
-        selected.html(defaultVal)
-      })
-    }
-  
-//     $('.select-default').click(function() {
-//       var defaultVal = $(this).data('default'),
-//           actualVal =$(this).data('value'),
-//           isActive = ($(this).hasClass('active')) ? true : false,
-//           selected = $(this).children('.select-default__selected');
-  
-//       if(isActive) {
-//         if(actualVal == "") {
-//           selected.html(defaultVal)
-//         } else {
-//           selected.html(actualVal)
-//         }
-//       } else {
-//         selected.html(defaultVal)
-//       }
-  
-//       $(this).toggleClass('active')
-//     })
-  
-//     $('.select-default__item').click(function() {
-//       var actualVal = $(this).html(),
-//           thisParent = $(this).parent().parent();
-  
-//       thisParent.data('value', actualVal)
-//     })
-//   })
+        selected.html( (realValue == '') ? defaultValue : realValue )
+    })
+}
+
+if($('.select-default').length > 0) {
+    initSelect()
+}
 
 // Select updated
-$('.select-default').click(function(e) {
+// const initSelectFunctions = function(s, e) {
+//     var selectDefault = s,
+//         defaultValue = selectDefault.data('default'),
+//         selected = selectDefault.find($('.select-default__selected'));
+
+//     if(selectDefault.hasClass('active')) {
+//         var elementClicked = $(e.target),
+//             inputHidden = selectDefault.find($('.select__hidden'));
+
+//             if(elementClicked.hasClass('select-default__item')) {
+//                 var newValue = elementClicked.html();
+//                 selected.html(newValue);
+//                 inputHidden.val(newValue);
+//                 selectDefault.removeClass('active');
+//             }
+
+//             // Cerrar si se hace click en selected
+//             if(elementClicked.hasClass('select-default__selected')) {
+//                 selectDefault.removeClass('active');
+//                 if(!inputHidden.val()) {
+//                     selected.html(defaultValue);
+//                 } else {
+//                     selected.html(inputHidden.val());
+//                 }
+//             }
+
+//     } else {
+//         selected.html(defaultValue);
+//         selectDefault.addClass('active');
+//     }
+// }
+
+// $('.select-default').click(function(e) {
+//     initSelectFunctions($(this), e)
+// })
+$(document).on('click', '.select-default', function(e) {
     var selectDefault = $(this),
         defaultValue = selectDefault.data('default'),
         selected = selectDefault.find($('.select-default__selected'));
@@ -142,7 +155,6 @@ $('.select-default').click(function(e) {
         selected.html(defaultValue);
         selectDefault.addClass('active');
     }
-
 })
 
 // Cerrar select desplegado si se hace click afuera
@@ -159,51 +171,8 @@ $(document).click(function(e) {
     }
 })
 
-// Agregar y quitar productos
-// var newProductItem = 
-
-// newProductRow += '<td>';
-// newProductRow += '<input class="field default block" type="text"/>';
-// newProductRow += '</td>';
-// newProductRow += '<td>';
-// newProductRow += '<div class="select-default block" data-default="Seleccionar" data-value="">';
-// newProductRow += '<div class="select-default__selected"></div>';
-// newProductRow += '<input class="select__hidden" type="hidden"/>';
-// newProductRow += '<div class="select-default__list">';
-// newProductRow += '<div class="select-default__item">Kg</div>';
-// newProductRow += '<div class="select-default__item">Mts</div>';
-// newProductRow += '</div>';
-// newProductRow += '</div>';
-// newProductRow += '</td>';
-// newProductRow += '<td>';
-// newProductRow += '<input class="field default block" type="text"/>';
-// newProductRow += '</td>';
-// newProductRow += '<td>';
-// newProductRow += '<input class="field default block" type="text"/>';
-// newProductRow += '</td>';
-// newProductRow += '<td>';
-// newProductRow += '<div class="select-default block" data-default="Seleccionar" data-value="">';
-// newProductRow += '<div class="select-default__selected"></div>';
-// newProductRow += '<input class="select__hidden" type="hidden"/>';
-// newProductRow += '<div class="select-default__list">';
-// newProductRow += '<div class="select-default__item">Tipo 1</div>';
-// newProductRow += '<div class="select-default__item">Tipo 2</div>';
-// newProductRow += '</div>';
-// newProductRow += '</div>';
-// newProductRow += '</td>';
-// newProductRow += '<td>';
-// newProductRow += '<input class="field default block" type="text"/>';
-// newProductRow += '</td>';
-// newProductRow += '<td>';
-// newProductRow += '<input class="field default block" type="text"/>';
-// newProductRow += '</td>';
-// newProductRow += '<td>';
-// newProductRow += '<input class="field default block" type="text"/>';
-// newProductRow += '</td>';
-// newProductRow += '<td><a class="delete-product" href="#"><img class="delete-product__icon" src="./img/trash.svg" alt=""/></a></td>';
-// newProductRow += '</tr>';
-
 createNewProduct = function() {
+    // 1
     var newRow = document.createElement('tr');
 
     var newCol1 = document.createElement('td');
@@ -212,38 +181,102 @@ createNewProduct = function() {
     input1.classList.add('field', 'default', 'block');
     newCol1.appendChild(input1);
     
+    // 2
     var newCol2 = document.createElement('td');
+    var select2 = document.createElement('div');
+    select2.setAttribute('data-default', 'Seleccionar')
+    select2.classList.add('select-default', 'block');
+
+    var select2Selected = document.createElement('div');
+    select2Selected.classList.add('select-default__selected');
+
+    var select2Input = document.createElement('input');
+    select2Input.setAttribute('type', 'hidden');
+    select2Input.classList.add('select__hidden');
+
+    var select2List = document.createElement('div');
+    select2List.classList.add('select-default__list');
+
+    var options = ['kg', 'mts'];
+    for(option in options) {
+        var selectOption = document.createElement('div');
+        selectOption.classList.add('select-default__item');
+        selectOption.innerHTML = options[option];
+
+        select2List.appendChild(selectOption);
+    }
+
+    select2.appendChild(select2Selected);
+    select2.appendChild(select2Input);
+    select2.appendChild(select2List);
+    newCol2.appendChild(select2);
     
-    
+    // 3
     var newCol3 = document.createElement('td');
     var input3 = document.createElement('input');
     input3.setAttribute('type', 'text');
     input3.classList.add('field', 'default', 'block');
     newCol3.appendChild(input3);
+
+    // 4
     var newCol4 = document.createElement('td');
     var input4 = document.createElement('input');
     input4.setAttribute('type', 'text');
     input4.classList.add('field', 'default', 'block');
     newCol4.appendChild(input4);
     
+    // 5
     var newCol5 = document.createElement('td');
+    var select5 = document.createElement('div');
+    select5.setAttribute('data-default', 'Seleccionar')
+    select5.classList.add('select-default', 'block');
 
+    var select5Selected = document.createElement('div');
+    select5Selected.classList.add('select-default__selected');
+
+    var select5Input = document.createElement('input');
+    select5Input.setAttribute('type', 'hidden');
+    select5Input.classList.add('select__hidden');
+
+    var select5List = document.createElement('div');
+    select5List.classList.add('select-default__list');
+
+    var options = ['Tipo 1', 'Tipo 2'];
+    for(option in options) {
+        var selectOption = document.createElement('div');
+        selectOption.classList.add('select-default__item');
+        selectOption.innerHTML = options[option];
+
+        select5List.appendChild(selectOption);
+    }
+
+    select5.appendChild(select5Selected);
+    select5.appendChild(select5Input);
+    select5.appendChild(select5List);
+    newCol5.appendChild(select5);
+
+    // 6
     var newCol6 = document.createElement('td');
     var input6 = document.createElement('input');
     input6.setAttribute('type', 'text');
     input6.classList.add('field', 'default', 'block');
     newCol6.appendChild(input6);
+
+    // 7
     var newCol7 = document.createElement('td');
     var input7 = document.createElement('input');
     input7.setAttribute('type', 'text');
     input7.classList.add('field', 'default', 'block');
     newCol7.appendChild(input7);
+
+    // 8
     var newCol8 = document.createElement('td');
     var input8 = document.createElement('input');
     input8.setAttribute('type', 'text');
     input8.classList.add('field', 'default', 'block');
     newCol8.appendChild(input8);
 
+    // 9
     var newCol9 = document.createElement('td');
     var deleteAnchor = document.createElement('a');
     deleteAnchor.setAttribute('href', '#');
@@ -268,13 +301,23 @@ createNewProduct = function() {
 }
 
 $('.new-product-row').click(function() {
+    // Agregar nueva columna de producto
     $('.table > tbody:last-child').append(createNewProduct);
+
+    // Iniciar selects de nueva columna
+    initSelect();
+    // $('.select-default').click(function(e) {
+    //     initSelectFunctions($(this), e)
+    // })
     
-    $('.delete-product').click(function() {
-        $(this).parent().parent().remove()
-    })
+    // Iniciar acción de botón eliminar
+    // $('.delete-product').click(function() {
+    //     $(this).parent().parent().remove()
+    // })
 })
 
-$('.delete-product').click(function() {
+$(document).on('click', '.delete-product', function() {
     $(this).parent().parent().remove()
 })
+// $('.delete-product').on('click', function() {
+// })
