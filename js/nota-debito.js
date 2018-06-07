a9aca65545e7743ec20d31b468cdda4113e21dbf
Ken Ramirez <kenramirezp@gmail.com>
2018-06-07 15:04:55 -0500

    Nota de débito funcionando

--- js/nota-debito.js
+++ js/nota-debito.js
@@ -1,12 +1,31 @@
 $(document).ready(function() {
-    const tipoNota = $('select[name=tipo-nota]'),
-        serieCod = $('input[name=serie-cod]')
+    const tipoDocumento = $('select[name=tipo-documento]'),
+        serieCod = $('input[name=serie-cod]'),
+        searchBtn = $('button[name=search]'),
+        spinner = $('.spinner')
 
-    tipoNota.change(function() {
-        updateSerie(tipoNota.val())
+    tipoDocumento.change(function() {
+        updateSerie(tipoDocumento.val())
+        $('input[name=serie]').prop('disabled', false)
     })
     
-    updateSerie = function(tipoNota) {
-        (tipoNota == 1) ? serieCod.val('F001') : serieCod.val('B001')
+    updateSerie = function(tipoDocumento) {
+        (tipoDocumento == 1) ? serieCod.val('F001') : serieCod.val('B001')
     }
+
+    $('input[name=serie]').on('keyup', function() { // Activar botón Buscar
+
+        if($(this).val() != '' && searchBtn.is(':disabled')) searchBtn.prop('disabled', false)
+        if($(this).val() == '') searchBtn.prop('disabled', true)
+    })
+
+    searchBtn.on('click', function() { // Validar serie
+        spinner.show()
+
+        setTimeout(function() {
+            spinner.hide()
+            searchBtn.html('Visualizar')
+            $('input[name=serie]').addClass('success')
+        }, 2000)
+    })
 })
